a9a5fd783c5c4d3df503923c5769b3a637ce792b
Vitaliy <vitaliyanohin@gmail.com>
2019-06-21 21:35:47 +0300

    dev

--- src/Animal.java
+++ src/Animal.java
@@ -2,18 +2,21 @@ import java.io.Serializable;
 import java.util.Objects;
 
 class Animal implements Serializable {
-    private final String name;
+  private final String name;
 
-    public Animal(String name) {
-        this.name = name;
-    }
+  public Animal(String name) {
+    this.name = name;
+  }
 
-    @Override
-    public boolean equals(Object obj) {
-        if (obj instanceof Animal) {
-            return Objects.equals(name, ((Animal) obj).name);
-        }
-        return false;
+  @Override
+  public boolean equals(Object obj) {
+    if (obj instanceof Animal) {
+      return Objects.equals(name, ((Animal) obj).name);
     }
-    public String getName() { return name; }
+    return false;
+  }
+
+  public String getName() {
+    return name;
+  }
 }
